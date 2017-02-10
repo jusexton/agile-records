670701c690058ddc6334c00a6f93892d7008ad4e
Justin Sexton <jsexton11295@gmail.com>
2017-02-09 19:42:35 -0600

    Restructured project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main.java;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,17 +7,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void stop() throws Exception{
+        // Clean up logic
+        super.stop();
     }
 }
---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,7 @@
+package main.java.controller;
+
+/**
+ */
+public class Controller {
+
+}
