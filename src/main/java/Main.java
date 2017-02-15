2e6d67b6ead65abb43cc8f6b5c9475ca5404d838
Justin Sexton <jsexton11295@gmail.com>
2017-02-14 22:53:58 -0600

    Updated comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 
     @Override
     public void stop() throws Exception {
-        // Clean up logic
+        // Clean up logic (if any)
         super.stop();
     }
 }
