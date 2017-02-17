b947cf7ad193c75dc4ada21315e151f708e745fb
German Villalobos <german.villalobos0@gmail.com>
2017-02-17 02:53:44 -0600

    Update for student
    Didn't get any errors for this. Let me know if you do. For set gpa I didnt add a parameter since gpa is calculated based on the grades for the courses.

--- src/main/java/users/Student.java
+++ src/main/java/users/Student.java
@@ -1,36 +1,48 @@
-package main.java.users;
+package main.java.users.student;
+
+import main.java.users.User;
+import main.java.util.security.Hash;
 
 /**
- * Represents a student on the sql server.
+ * Class that represents students on the sql server.
  */
-public class Student extends User{
+public class Student extends User {
+    public Student(String userName, Hash password, int ID) {
+        super(userName, password, ID);
+    }
 
-    private double gpa;
+    private double GPA;
     private List<Course> registeredCourses;
 
-    public void addCourse(int crn, string name,List<Double> g, int h)
+    public void addCourse(int CRN, string name,List<Double> grades, int creditHours)
     {
-        c = new Course(crn, name, g, h);
-        registeredCourses.add(c);
-        delete c;
+        course = new Course(CRN, name, grades, creditHours);
+        registeredCourses.add(course);
     }
+
     public void setGPA(){
         int size = registeredCourses.size();
         int totalHours = 0;
         double total = 0;
         for(int index = 0; index < size; index++) {
-            h = registeredCourses.index.getCreditHours();
+            hours = registeredCourses.index.getCreditHours();
             total += registeredCourses.index.getClassAvg()*h;
-            totalHours +=h;
+            totalHours +=hours;
         }
-        gpa = total/totalHours;
+        this.GPA = total/totalHours;
 
     }
     public double getGPA(){
-        return gpa;
+        return GPA;
+    }
+
+    public List<Course> getRegisteredCourses(){
+        return registereCourses
+    }
+
+    public void setRegisteredCourses(List<Course> courses){
+        this.registeredCourses = courses;
     }
 
 
-
 }
-
