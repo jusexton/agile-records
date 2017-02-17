b17ea729826f473e55eefae33fe0568ace74e5c4
German Villalobos <german.villalobos0@gmail.com>
2017-02-17 03:50:11 -0600

    Update Student.java
    Alright hopefully this is the last time I have to add it. This is the actual file i meant to upload. Didn't mean to stress you out with all my errors. Also if i messed up with the syncing I just want to post this so its not completely deleted. Is that the correct way to access the gethours function from each list element?

--- src/main/java/users/students/Student.java
+++ src/main/java/users/students/Student.java
@@ -7,7 +7,36 @@ import main.java.util.security.Hash;
  * Class that represents students on the sql server.
  */
 public class Student extends User {
+    private double GPA;
+    private List<Course> registeredCourses;
+    
     public Student(String userName, Hash password) {
         super(userName, password);
     }
+
+    public void addCourse(String name, int creditHours, int CRN, List<Double> grades, LocalDateTime time){
+        course = new Course(String name, int creditHours, int CRN);
+        course.setGrades(grades);
+        course.setTime(time);
+        this.registeredCourses.add(course);
+
+    }
+    public double getGPA(){return GPA}
+    void setGPA(){
+        int size = this.registeredCourses.size();
+        int hours = 0;
+        int totalHours = 0;
+        double total = 0;
+        for(int index = 0; index < size; index++)
+        {
+            hours = this.registeredCourses.get(index).getCreditHours();
+            total += this.registeredCourses.get(index).getAverage() * hours;
+            totalHours += hours;
+        }
+        this.GPA =  total/totalHours;
+    }
+    public return List<course> getRegisteredCourses(){ return registeredCourses}
+    public void setRegisteredCourses(List<course> courses){ this.registeredCourses = courses}
+
+
 }
