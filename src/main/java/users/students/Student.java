044382843fbdb325cd434ba3cadc0282c137cd41
Justin Sexton <jsexton11295@gmail.com>
2017-02-17 20:01:11 -0600

    Updated Student Class

--- src/main/java/users/students/Student.java
+++ src/main/java/users/students/Student.java
@@ -3,40 +3,37 @@ package main.java.users.students;
 import main.java.users.User;
 import main.java.util.security.Hash;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class that represents students on the sql server.
  */
 public class Student extends User {
-    private double GPA;
-    private List<Course> registeredCourses;
-    
+    private List<Course> courses;
+
     public Student(String userName, Hash password) {
         super(userName, password);
+        courses = new ArrayList<>();
     }
 
-    public void addCourse(String name, int creditHours, int CRN, List<Double> grades, LocalDateTime time){
-        course = new Course(name, creditHours, CRN);
-        course.setGrades(grades);
-        course.setTime(time);
-        this.registeredCourses.add(course);
-
-    }
-    public double getGPA(){return GPA}
-    void setGPA(){
-        int size = this.registeredCourses.size();
+    public double getGPA() {
         int hours = 0;
         int totalHours = 0;
         double total = 0;
-        for(int index = 0; index < size; index++)
-        {
-            hours = this.registeredCourses.get(index).getCreditHours();
-            total += this.registeredCourses.get(index).getAverage() * hours;
+        for (Course registeredCourse : this.courses) {
+            hours = registeredCourse.getCreditHours();
+            total += registeredCourse.getAverage() * hours;
             totalHours += hours;
         }
-        this.GPA =  total/totalHours;
+        return total / totalHours;
     }
-    public return List<Course> getRegisteredCourses(){ return registeredCourses}
-    public void setRegisteredCourses(List<Course> courses){ this.registeredCourses = courses}
 
+    public List<Course> getCourses() {
+        return courses;
+    }
 
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
 }
