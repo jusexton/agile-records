c4b6857aa3d4815f93bbe8512208293224a204de
schultzms <mike@schultz.ms>
2017-02-26 13:03:49 -0600

    Updated Tests

--- src/tests/groovy/DatabaseTests.groovy
+++ src/tests/groovy/DatabaseTests.groovy
@@ -2,6 +2,11 @@ package tests.groovy
 
 import main.java.util.database.DatabaseUtil
 
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import java.sql.Connection
 
 /**
@@ -9,13 +14,34 @@ import java.sql.Connection
  */
 class DatabaseTests extends GroovyTestCase {
     // Test passes
-    void testConnection() {
+    public static Connection getConnection() {
         final String host = "jdbc:mysql://gator4196.hostgator.com:3306/txscypaa_agilerecords"
         final String password = "txscypaa_agile"
         final String dbName = "4@lq^tsFiI0b"
 
         Connection connection = DatabaseUtil.getConnection(host, password, dbName)
-        assertNotNull connection
+        assertNotNull(connection)
+
+        return connection;
     }
+
+    void addStudent(){
+
+
+    }
+
+   void testQuery()
+    {
+
+        Statement stmt=connection.createStatement();
+
+        ResultSet rs=stmt.executeQuery("select * from users");
+
+        while(rs.next())
+            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "+rs.getString(5)+"  "+rs.getString(6));
+        connection.close();
+
+    }
+
 
 }
