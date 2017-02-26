a3575879a6cd229accee157670ce6d200e7893d3
schultzms <mike@schultz.ms>
2017-02-26 17:43:36 -0600

    Updated Tests

--- src/tests/groovy/DatabaseTests.groovy
+++ src/tests/groovy/DatabaseTests.groovy
@@ -1,6 +1,14 @@
 package tests.groovy
 
-import main.java.util.database.DatabaseUtil
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import main.java.users.Admin
+import main.java.users.students.Course
+import main.java.users.students.Major
+import main.java.users.students.Student
+import main.java.util.database.SQLConnection
+import main.java.util.security.Hash
+import main.java.util.security.HashingUtil
 
 import java.sql.ResultSet;
 import java.sql.Connection;
@@ -13,35 +21,140 @@ import java.sql.Connection
  * Contains all tests involving the database
  */
 class DatabaseTests extends GroovyTestCase {
-    // Test passes
-    public static Connection getConnection() {
-        final String host = "jdbc:mysql://gator4196.hostgator.com:3306/txscypaa_agilerecords"
-        final String password = "txscypaa_agile"
-        final String dbName = "4@lq^tsFiI0b"
 
-        Connection connection = DatabaseUtil.getConnection(host, password, dbName)
+    final String host = "jdbc:mysql://gator4196.hostgator.com:3306/txscypaa_agilerecords"
+    final String password = "txscypaa_agile"
+    final String dbName = "4@lq^tsFiI0b"
+
+    Student createTestStudent() {
+
+        Hash hash  = HashingUtil.hash("123456","SHA-256")
+        Student testStudent = new Student("mschultz", hash);
+
+        List courses = new ArrayList();
+        courses.add(new Course("CS 3420", 3, 10426));
+        courses.add(new Course("CS 3306", 3, 10515));
+        courses.add(new Course("CS 2410", 4, 10818));
+
+        testStudent.setCourses(courses);
+
+        testStudent.setMajor(Major.ComputerScience);
+
+        testStudent.setEmail("mike@schultz.ms");
+
+        testStudent.setFirstName("Mike");
+        testStudent.setLastName("Schultz");
+        testStudent.setID(999);
+
+        return testStudent;
+
+    }
+
+    Admin createTestAdmin() {
+
+        Admin testAdmin = new Admin("adminuser", new Hash("111111", "222222", "333333"));
+
+
+        testAdmin.setEmail("admin@schultz.ms");
+
+        testAdmin.setFirstName("Admin");
+        testAdmin.setLastName("User");
+        testAdmin.setID(123);
+
+        return testAdmin;
+
+    }
+
+
+    public void getConnection() {
+
+
+        Connection connection = SQLConnection.establishConnection(host, password, dbName)
         assertNotNull(connection)
 
-        return connection;
     }
 
-    void addStudent(){
+    void testAddStudent() {
 
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
 
-    }
-
-   void testQuery()
-    {
-
-        Statement stmt=connection.createStatement();
-
-        ResultSet rs=stmt.executeQuery("select * from users");
-
-        while(rs.next())
-            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "+rs.getString(5)+"  "+rs.getString(6));
-        connection.close();
+        testConnection.addUser(createTestStudent())
 
     }
 
+    void testAddAdmin() {
 
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
+
+        testConnection.addUser(createTestAdmin())
+
+    }
+
+    void testGetStudent() {
+
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
+
+        testConnection.getStudent(24);
+
+    }
+
+    void testGetNextID() {
+
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
+
+        int id =
+        testConnection.getNextID();
+
+        System.out.println(id)
+
+
+    }
+
+    void testGetUser() {
+
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
+
+        testConnection.getUser(24);
+
+
+    }
+
+
+
+    void testGetAllStudents() {
+
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
+
+        List<Student> allStudents = testConnection.getAllStudents();
+
+        assertEquals(allStudents.size(), 3)
+
+    }
+
+
+    void testUpdateStudent() {
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
+
+        testConnection.updateStudent(24, createTestStudent());
+
+    }
+    void testAddUsers(){
+
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
+
+        testConnection.addUser(createTestStudent())
+       // testConnection.addUser(createTestAdmin())
+    }
+
+    void testDoLogin(){
+
+        SQLConnection testConnection = new SQLConnection(host, password, dbName);
+
+        Student theStudent = testConnection.doLogin("mschultz","123456")
+
+        System.out.println(theStudent.getFirstName())
+
+
+
+    }
 }
