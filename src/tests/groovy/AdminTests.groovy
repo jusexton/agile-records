20fd59ffcd5b273985a48f6db017bf8011b5557b
Justin Sexton <jsexton11295@gmail.com>
2017-02-28 22:39:18 -0600

    Tested Admin class

--- src/tests/groovy/AdminTests.groovy
+++ src/tests/groovy/AdminTests.groovy
@@ -1,8 +1,23 @@
 package tests.groovy
 
+import main.java.users.Admin
+import main.java.users.students.Student
+import main.java.util.security.Hash
+import main.java.util.security.HashingUtil
+
 /**
  * Class that will contain all tests regarding the Admin class.
  */
 class AdminTests extends GroovyTestCase {
+    static Admin getTestAdmin(){
+        Hash hash = HashingUtil.hash("password", "SHA-256")
+        return new Admin("admin", hash)
+    }
 
+    // Passed
+    void testInstance(){
+        Admin testAdmin = getTestAdmin()
+        assertTrue(testAdmin instanceof Admin)
+        assertFalse(testAdmin instanceof Student)
+    }
 }
