05eddaf870fc21b5065cca172c8fd3d0bc9e839f
Justin Sexton <jsexton11295@gmail.com>
2017-03-21 15:27:57 -0500

    Updated comments

--- src/script/DatabasePopulator.groovy
+++ src/script/DatabasePopulator.groovy
@@ -48,6 +48,7 @@ static Grade generateGrade() {
 static void populateDatabase(int count) {
     try {
         SQLConnection connection = new SQLConnection()
+        // Add count number of students.
         count.times {
             connection.addUser(generateStudent())
         }
@@ -56,5 +57,5 @@ static void populateDatabase(int count) {
     }
 }
 
-// Will place 5 randomly generated users in database.
+// Places 5 randomly generated users in database.
 // populateDatabase(5)
